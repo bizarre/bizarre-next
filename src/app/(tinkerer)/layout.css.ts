63c598bbe97cce8ad5ad138a06947e2a936c60b0
Alexander Maxwell Adewole <alex@bizar.re>
2022-12-23 11:36:25 -0800

    Add back top padding + margin on desktop for two-pane content

--- src/app/(tinkerer)/layout.css.ts
+++ src/app/(tinkerer)/layout.css.ts
@@ -1,4 +1,13 @@
 import { style } from "@vanilla-extract/css";
 import theme from "@/theme";
 
-export const wrapper = style({});
+export const wrapper = style({
+  marginTop: theme.vars.text.size.lg,
+  paddingTop: theme.vars.text.size.lg,
+  "@media": {
+    "screen and (max-width: 1024px)": {
+      paddingTop: 0,
+      marginTop: 0,
+    },
+  },
+});
